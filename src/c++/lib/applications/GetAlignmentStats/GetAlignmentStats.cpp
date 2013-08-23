d7eb495c7458f8cd1511ef1e462c05d627006e58
xchen <xchen@xchen-VirtualBox.(none)>
2013-08-22 17:35:00 -0700

    test

--- src/c++/lib/applications/GetAlignmentStats/GetAlignmentStats.cpp
+++ src/c++/lib/applications/GetAlignmentStats/GetAlignmentStats.cpp
@@ -51,7 +51,7 @@ runAlignmentStats(const AlignmentStatsOptions& opt)
     {
         ReadGroupStats rgs = ReadGroupStats(file);
     	rstats.setStats(file,rgs);
-
+        /*
     	// debug...
     	std::ofstream outSerialized(serizedStatsFile);
     	boost::archive::text_oarchive oa(outSerialized);
@@ -63,11 +63,13 @@ runAlignmentStats(const AlignmentStatsOptions& opt)
     	ia >> rgsNew;
 
     	rstatsDeserized.setStats(file, rgsNew);
+    	*/
     }
 
     rstats.write(outs.getStream());
     // debug...
-    rstatsDeserized.write(outs.getStream());
+    //rstatsDeserized.write(outs.getStream());
+
 }
 
 
